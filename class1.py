7ffbf747aab256dc016bb6da2698d5b3b9f74551
Amal Malmat <15amalmat@dunraven.det.org.uk>
2022-04-05 14:35:36 +0100

    .

--- class1.py
+++ class1.py
@@ -1,49 +1,74 @@
 import math 
 
-def check_vertex(CoordinateSet1, CoordinateSet2): #trace table needed, appears not to work
+def check_vertex(CoordinateSet1, CoordinateSet2): #to check if the lines between two sets of coordinates intersect
+  
     x1_1 = CoordinateSet1[0][0]
     x1_2 = CoordinateSet1[1][0]
     y1_1 = CoordinateSet1[0][1]
     y1_2 = CoordinateSet1[1][1]
 
-    x2_1 = CoordinateSet2[0][0]
-    x2_2 = CoordinateSet2[1][0]
-    y2_1 = CoordinateSet2[0][1]
-    y2_2 = CoordinateSet2[1][1]
-    #zero error to be dealt with
     try:
         m1 = (y1_2 - y1_1)/(x1_2 - x1_1)
     except ZeroDivisionError:
         m1 = math.inf
 
-    try:
-        m2 = (y2_2 - y2_1)/(x2_2 - x2_1)
-    except ZeroDivisionError:
-        m2 = math.inf
+    if isinstance(CoordinateSet2[0], list):
+        x2_1 = CoordinateSet2[0][0]
+        x2_2 = CoordinateSet2[1][0]
+        y2_1 = CoordinateSet2[0][1]
+        y2_2 = CoordinateSet2[1][1]
+        #zero error to be dealt with
+
+
+        try:
+            m2 = (y2_2 - y2_1)/(x2_2 - x2_1)
+        except ZeroDivisionError:
+            m2 = math.inf
     
-    if m1 == m2:
-        """used for testing
-        print('gradients the same') 
-        """
+        if m1 == m2:
+            """used for testing
+            print('gradients the same') 
+            """
+            return False
+
+        if math.isinf(m1):
+            x = x1_1
+            y = (m2*(x - x2_1)) + y2_1
+        elif math.isinf(m2):
+            x = x2_1
+            y = (m1*(x - x1_1)) + y1_1
+        else:
+            x = (y2_1-y1_1+(m1*x1_1)-(m2*x2_1))/(m1 - m2) 
+            y = (m1*(x - x1_1)) + y1_1
+
+        if  (min(x1_1,x1_2) <= x <= max(x1_1,x1_2)) and (min(x2_1,x2_2) <= x <= max(x2_1,x2_2)) and (min(y1_1,y1_2) <= y <= max(y1_1,y1_2)) and (min(y2_1,y2_2) <= y <= max(y2_1,y2_2)):
+            return (round(x, 2),round(y, 2))
+        else:
+            """used for testing
+            print('intersections outside coordinate ranges')
+            """
+            return False
+    elif isinstance(CoordinateSet2[0], float) or isinstance(CoordinateSet2[0], int): #to check if a third coordinate is between two #to be tested
+        if (min(x1_1,x1_2) <= CoordinateSet2[0] <= max(x1_1,x1_2)) and ((min(y1_1,y1_2) <= CoordinateSet2[1] <= max(y1_1,y1_2))):
+            #if the third coordinate is between the other two coordinates
+            try:
+                m2 = (CoordinateSet2[1] - y1_1)/(CoordinateSet2[0] - x1_1)
+            except ZeroDivisionError:
+                m2 = math.inf
+
+            if (m1 == m2) or (CoordinateSet2 == tuple(CoordinateSet1[0])) or (CoordinateSet2 == tuple(CoordinateSet1[1])):
+                return True
+
+                """used for testing"""
+            elif (m1 != m2):
+                print('gradients not the same')
+            
+            """used for testing"""
+        else:
+            print('(third) coordinate outside coordinate ranges')
+            """"""
         return False
 
-    if math.isinf(m1):
-        x = x1_1
-        y = (m2*(x - x2_1)) + y2_1
-    elif math.isinf(m2):
-        x = x2_1
-        y = (m1*(x - x1_1)) + y1_1
-    else:
-        x = (y2_1-y1_1+(m1*x1_1)-(m2*x2_1))/(m1 - m2) 
-        y = (m1*(x - x1_1)) + y1_1
-
-    if  (min(x1_1,x1_2) <= x <= max(x1_1,x1_2)) and (min(x2_1,x2_2) <= x <= max(x2_1,x2_2)) and (min(y1_1,y1_2) <= y <= max(y1_1,y1_2)) and (min(y2_1,y2_2) <= y <= max(y2_1,y2_2)):
-        return (round(x, 2),round(y, 2))
-    else:
-        """used for testing
-        print('intersections outside coordinate ranges')
-        """
-        return False
 
 class map:
     
@@ -131,14 +156,17 @@ class map:
     def make_graph(self):
         self.get_nodes()
         self.graph = {EachNode:[] for EachNode in self.Nodes}
-        self.get_connections()
+      #  self.get_connections()
 
-"""used for testing"""
+      
+print(check_vertex(((8,6),(12,14)),(12,14)))
+
+"""used for testing
 Map_Instance = map([[[20,30],[50,30],[130,50],[110,70],[80,80],[70,70],[80,40],[100,20],[120,10]],[[30,50],[20,30],[40,10],[50,50],[20,30],[10,10]]])
 Map_Instance.make_graph()
 print(Map_Instance.graph)
 print(Map_Instance.Nodes)
-""""""           
+"""           
 
 
     
